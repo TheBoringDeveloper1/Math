2eb918332553e937c4e116ad5351a3da50bf7978
Devvx <manuelcacciamani1@gmail.com>
2026-09-12 16:45:13 +0200

    Add files via upload

---
+++ RadiceQuadrata/radiceQuadrata.cpp
@@ -0,0 +1,27 @@
+// Includo le direttive
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Funzione principale(main) 
+
+int main() {
+
+    // Dichiarazione variabili utili al programma
+
+    int num;
+    int risultato;
+
+    // Inserimento numero
+
+    cout << "Inserisci numero: ";
+    cin >> num;
+
+    // Risultato
+
+    risultato = sqrt(num);
+    cout << "\nRisultato: " << risultato;
+    
+}
