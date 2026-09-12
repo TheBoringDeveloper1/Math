ea4aed2af8aa1aef38907399141115ab722a567b
Devvx <manuelcacciamani1@gmail.com>
2026-09-12 20:12:01 +0200

    Add files via upload

---
+++ Seno/seno.cpp
@@ -0,0 +1,27 @@
+// Includo le direttive
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Dichiarazione variabili utili al programma
+
+    double num;
+    double risultato;
+
+    // Inserimento numero
+
+    cout << "Inserisci il numero: ";
+    cin >> num;
+
+    // Risultato
+
+    risultato = sin(num);
+    cout << "\nSeno: " << risultato;
+
+}
