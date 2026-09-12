9adf7682da11bedc527788b0e896358b40f27222
Devvx <manuelcacciamani1@gmail.com>
2026-09-12 16:39:52 +0200

    Add files via upload

---
+++ Addizione/addizione.cpp
@@ -0,0 +1,32 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Dichiarazione variabili utili al programma
+
+    int num1;
+    int num2;
+
+    int risultato;
+
+    // Inserimento primo numero
+
+    cout << "Inserisci il primo numero: ";
+    cin >> num1;
+
+    // Inserimento secondo numero
+
+    cout << "\nInserisci il secondo numero: ";
+    cin >> num2;
+
+    // Risultato
+
+    risultato = num1 + num2;
+    cout << "\nRisultato: " << risultato;
+    
+}
